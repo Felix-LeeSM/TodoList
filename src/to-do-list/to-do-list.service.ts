94e9257b3147fa7838fd6b2e9391dd1623928e90
Seungmin <haradwaith@naver.com>
2022-04-10 01:41:11 +0900

    [init] todo list

--- src/to-do-list/to-do-list.service.ts
+++ src/to-do-list/to-do-list.service.ts
@@ -3,16 +3,18 @@ import {
   BadRequestException,
   ForbiddenException,
   Injectable,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateToDoListDto } from './dto/create-to-do-list.dto';
-import { Repository } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 
 @Injectable()
 export class ToDoListService {
   constructor(
     @InjectRepository(ToDos)
     private readonly toDosRepository: Repository<ToDos>,
+    private readonly connection: Connection,
   ) {}
 
   async createToDo(userId: string, createToDoListDto: CreateToDoListDto) {
@@ -64,23 +66,38 @@ export class ToDoListService {
   }
 
   async deleteOne(userId: string, id: number) {
+    let toDo: ToDos;
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
-      const toDo = await this.toDosRepository.findOneOrFail({
+      toDo = await this.toDosRepository.findOneOrFail({
         where: { userId, id },
         select: ['id', 'deletedAt', 'sequence'],
       });
-
-      await this.toDosRepository
+    } catch (err) {
+      throw new ForbiddenException('No Such Todo');
+    }
+    try {
+      await queryRunner.manager
+        .getRepository(ToDos)
         .createQueryBuilder()
         .update()
         .set({ sequence: () => 'sequence - 1' })
-        .where('sequence > :sequence', { sequence: toDo.sequence });
+        .where('sequence > :sequence', { sequence: toDo.sequence })
+        .execute();
       toDo.deletedAt = new Date();
       toDo.sequence = 0;
-      const result = await this.toDosRepository.save(toDo);
-      return result;
+      await queryRunner.manager.getRepository(ToDos).save(toDo);
+      await queryRunner.commitTransaction();
     } catch (err) {
-      throw new ForbiddenException('Not Your Todo');
+      toDo = null;
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+      if (toDo) return toDo;
+      throw new InternalServerErrorException('Please Try Again');
     }
   }
 
@@ -164,6 +181,8 @@ export class ToDoListService {
   }
 
   async changeSequence(userId: string, id: number, To: number) {
+    let from: number;
+    let to: number;
     try {
       const [prep, next] = await Promise.all([
         this.toDosRepository.findOne({
@@ -181,36 +200,55 @@ export class ToDoListService {
           select: ['sequence'],
         }),
       ]);
-      const [from, to] = [prep.sequence, next.sequence];
-      if (from < to) {
-        await this.toDosRepository
+      [from, to] = [prep.sequence, next.sequence];
+    } catch (err) {
+      console.log(err);
+      throw new BadRequestException('Bad Request');
+    }
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      if (from === to) {
+        return { affected: 0 };
+      } else if (from < to) {
+        await queryRunner.manager
+          .getRepository(ToDos)
           .createQueryBuilder()
           .update()
           .set({ sequence: () => 'sequence - 1' })
           .where('userId = :userId', { userId })
           .andWhere('sequence > :from', { from })
           .andWhere('sequence <= :to', { to })
+          .andWhere('deletedAt IS NULL')
           .execute();
       } else {
-        await this.toDosRepository
+        await queryRunner.manager
+          .getRepository(ToDos)
           .createQueryBuilder()
           .update()
           .set({ sequence: () => 'sequence + 1' })
           .where('userId = :userId', { userId })
           .andWhere('sequence < :from', { from })
           .andWhere('sequence >= :to', { to })
+          .andWhere('deletedAt IS NULL')
           .execute();
       }
-      const result = await this.toDosRepository
+      await queryRunner.manager
+        .getRepository(ToDos)
         .createQueryBuilder()
         .update()
         .set({ sequence: to })
         .where('userId = :userId')
         .andWhere('sequence = :from', { from })
         .execute();
-      return result;
+      await queryRunner.commitTransaction();
+      return true;
     } catch (err) {
-      throw new BadRequestException('bad request');
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+      throw new InternalServerErrorException('Please Try Again');
     }
   }
 }
